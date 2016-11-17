1483a1855c6a65769d977a6bb9349b92e83c97a3
Regaler <akalsdnr@kaist.ac.kr>
2016-11-17 11:26:31 +0900

    Chosung-first style.
    This's chosung-first style, which is not in vogue so much though.
    Just for academic purpose.

--- TOTALPLAN.py
+++ TOTALPLAN.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 import MealyMachine
 import sys
-import Chain
+import Chain2
 
 f_mm = open(sys.argv[1], "r")
 mealy = MealyMachine.makeMealyMachine(f_mm)
@@ -21,14 +21,16 @@ if effectiveness == True:
     #print "MMoutput: ", MMoutput
     #print "chain: ", chain
     #print("VALID!!!!!")
-    
-    marked_chain = Chain.position_marker(chain, MMoutput)	# rhkwkfmfajrwk->r1h2k2w1k2f1m2f3a1j2r3w1k2
-    #print "marked_chain: ", marked_chain
-    codonized_chain = Chain.codonize(marked_chain)			# [r1h2k2][w1k2][f1m2f3][a1j2r3][w1k2]
-    processed_codonized_chain = Chain.process_del(codonized_chain,backspace)
-    letterized_chain = Chain.letterize_chain(processed_codonized_chain)	# ['과','자','를','먹','자']
+    print "chain: ", chain, " backspace: ", backspace
+    marked_chain = Chain2.position_marker(chain, MMoutput)	# rhkwkfmfajrwk->r1h2k2w1k2f1m2f3a1j2r3w1k2
+    print "marked_chain: ", marked_chain
+    codonized_chain = Chain2.codonize(marked_chain)			# [r1h2k2][w1k2][f1m2f3][a1j2r3][w1k2]
+    print "codonized_chain: ", codonized_chain
+    processed_codonized_chain = Chain2.process_del(codonized_chain,backspace)
+    print "processed_codonized_chain: ", processed_codonized_chain
+    letterized_chain = Chain2.letterize_chain(processed_codonized_chain)	# ['과','자','를','먹','자']
     #print letterized_chain
-    Chain.sentence_printer(letterized_chain)					# >> 과자를먹자
+    Chain2.sentence_printer(letterized_chain)					# >> 과자를먹자
 else:
     print("Invalid Hangeul")
 
